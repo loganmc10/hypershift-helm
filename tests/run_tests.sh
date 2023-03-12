1525559645dffbaf66825a8558060db0d3049007
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2023-03-11 20:41:54 -0700

    add tests (#3)

---
+++ tests/run_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function handle_error {
+    echo "Failed" >&2
+    exit 1
+}
+
+trap handle_error ERR
+
+for file in tests/configs/*; do
+  echo "Testing $(basename "${file}")"
+  test=$(helm template chart --values "${file}")
+  expected=$(cat "tests/results/$(basename "${file}")")
+  diff  <(echo "${test}" ) <(echo "${expected}")
+done
+
+echo Passed
