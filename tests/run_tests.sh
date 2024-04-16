1a5014fdec5029684ad77fb98697add8c24aae8b
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2024-04-16 16:39:40 -0600

    more

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -9,8 +9,8 @@ trap handle_error ERR
 
 for file in tests/configs/*; do
   echo "Testing $(basename "${file}")"
-  test=$(helm template chart --values "${file}")
-  expected=$(cat "tests/results/$(basename "${file}")")
+  test=$(helm template chart --values "${file}" | sed '/^#/d')
+  expected=$(sed '/^#/d' "tests/results/$(basename "${file}")" )
   helm lint --quiet --strict chart --values "${file}"
   diff  <(echo "${test}" ) <(echo "${expected}")
 done
